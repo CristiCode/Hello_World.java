2f995e423d0fea9aab0e72c4dfb1ca0933409a6e
Ninthiem <59974081+Ninthiem@users.noreply.github.com>
2020-01-16 19:16:49 +0100

    First Commit

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,16 @@
+/*
+ * Here's my first project called "Hello World!!!"
+ */
+package helloworld;
+
+/**
+ *
+ * @author Ninthiem
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("----Hello World!!!-----");
+    }
+    
+}
